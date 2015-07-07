830ee3754465db48e44e9d8a485d10a157d1c082
HomerJSimpson1 <vuke93@gmx.com>
2015-07-07 12:42:17 -0400

    Latest changes in freqarray.py

--- freqarray.py
+++ freqarray.py
@@ -11,12 +11,31 @@ def gen_freq_array(alphabet, k):
     """
 
     # There are (len(alphabet))^k - 1 possible k-mers.
-    #left = [ [alphabet[i]] * len(alphabet)^k/len(alphabet) for i in range(len(alphabet)) ]
-    left = [ [alphabet[i]] * ((len(alphabet))^(k-1)) for i in range(len(alphabet)) ]
+    #left = [ [alphabet[i]] * len(alphabet)**k/len(alphabet) for i in range(len(alphabet)) ]
+    left = [ [alphabet[i]] * ((len(alphabet))**(k-1)) for i in range(len(alphabet)) ]
     left1 = sum(left, [])
 
     # Have to figure out how to get the middle values, and when I don't know how long
     # the alphabet is.
 
+    alphalen = len(alphabet)
+    #print(alphalen, k)
+    #print(alphalen**k)
 
-    right = alphabet * ((len(alphabet))^(k-1))
+    right = alphabet * (alphalen**(k-1))
+
+    mylist = []
+    temp = ''
+    #for i in range(alphalen**(k-1)):
+    for i in range(alphalen**k):
+        for j in range(k):
+            temp += alphabet[int(i/(4**j)) % alphalen]
+        mylist.append(temp)
+        temp = ''
+
+    #print(mylist)
+
+    mydict = { mylist[i]:0 for i in range(len(mylist)) }
+
+    print(mydict)
+    return(mydict)
