204dbef8f7d59cff38cd4efdf0b62a090b1734e9
James Peterson <james@recruitcom.com>
2015-07-07 00:41:30 -0400

    Latest commit added freqarray.py

---
+++ freqarray.py
@@ -0,0 +1,22 @@
+# Filename: freqarray.py
+
+DNA_ALPHABET = ['A', 'B', 'C', 'D']
+
+def gen_freq_array(alphabet, k):
+    """
+    Bioinformatics Algorithms 1 (2015) Coursera Class.  Interactive Text Code Challenge
+    in the charging station topic, "The Frequency Array."
+    Input: The alphabet of symbols to use and the length k of each k-mer.
+    Output: A frequency array initialized to each possible k-mer with a frequency of 0.
+    """
+
+    # There are (len(alphabet))^k - 1 possible k-mers.
+    #left = [ [alphabet[i]] * len(alphabet)^k/len(alphabet) for i in range(len(alphabet)) ]
+    left = [ [alphabet[i]] * ((len(alphabet))^(k-1)) for i in range(len(alphabet)) ]
+    left1 = sum(left, [])
+
+    # Have to figure out how to get the middle values, and when I don't know how long
+    # the alphabet is.
+
+
+    right = alphabet * ((len(alphabet))^(k-1))
